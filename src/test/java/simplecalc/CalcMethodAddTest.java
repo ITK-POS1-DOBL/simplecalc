660cd5543d6355e6f996d50e09ba18b05c2ad192
Intruder <intruder1912@gmail.com>
2015-10-17 23:10:34 +0200

    added test folder and a first rudimentary test case

---
+++ src/test/java/simplecalc/CalcMethodAddTest.java
@@ -0,0 +1,14 @@
+package simplecalc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcMethodAddTest {
+
+	@Test
+	public void testDoCalculation() {
+		fail("Not yet implemented");
+	}
+
+}
